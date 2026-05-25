bb1a7eef16eff2b60fc99eda1be9bd71b42a079a
Chetan Kesare <chetan.kesare@clouddrove.com>
2026-05-25 22:32:50 +0530

    fix: Placeholders Fixed for ARN Roles.

--- main.tf
+++ main.tf
@@ -50,7 +50,7 @@ resource "aws_vpc_peering_connection" "default" {
   tags = merge(
     module.labels.tags,
     {
-      "Name" = module.labels.id
+      "Name" = format("%s-peering", module.labels.id)
     }
   )
 }
@@ -104,7 +104,7 @@ resource "aws_vpc_peering_connection_accepter" "peer" {
   tags = merge(
     module.labels.tags,
     {
-      "Name" = module.labels.id
+      "Name" = format("%s-peering", module.labels.environment)
     }
   )
 }
@@ -204,7 +204,7 @@ resource "aws_vpc_peering_connection" "region" {
   tags = merge(
     module.labels.tags,
     {
-      "Name" = module.labels.id
+      "Name" = format("%s-peering", module.labels.environment)
     }
   )
 }
